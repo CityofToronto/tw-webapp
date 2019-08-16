cce493adc1574506e591dced1ecb8f01ca01a490
Amber Brasher <abrashe@toronto.ca>
2019-08-16 10:40:19 -0400

    Added typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,29 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: [
+     '@typescript-eslint'
+  ],
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
+    'airbnb-base',
+    'plugin:vue/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    //'@vue/typescript',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-extra-semi': 'off',
-    "no-useless-constructor": "off"
+    "no-useless-constructor": "off",
+    "indent": "off",
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "multiline-comment-style": ["error", "starred-block"]
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
