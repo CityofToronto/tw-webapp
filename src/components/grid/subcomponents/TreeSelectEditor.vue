8a9b5ce7b0acc15f240b0e8fcd46a634992d24a8
Amber Brasher <abrashe@toronto.ca>
2019-08-20 11:59:43 -0400

    Created grid treeview editor

--- src/components/grid/subcomponents/TreeSelectEditor.vue
+++ src/components/grid/subcomponents/TreeSelectEditor.vue
@@ -1,11 +1,9 @@
 <template>
   <v-treeview
     v-model="selectedItems"
-    :items="params.list"
+    :items="params.values"
     selection-type="leaf"
-  >
-
-  </v-treeview>
+  />
 </template>
 
 <script lang="ts">
@@ -14,27 +12,6 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component({})
 export default class TreeSelectEditor extends Vue {
   selectedItems: string[] = [];
-
-  get listItems()
 }
 
 </script>
-
-// items: [
-//         {
-//           id: 1,
-//           name: 'Root',
-//           children: [
-//             { id: 2, name: 'Child #1' },
-//             { id: 3, name: 'Child #2' },
-//             {
-//               id: 4,
-//               name: 'Child #3',
-//               children: [
-//                 { id: 5, name: 'Grandchild #1' },
-//                 { id: 6, name: 'Grandchild #2' },
-//               ],
-//             },
-//           ],
-//         },
-//       ],
