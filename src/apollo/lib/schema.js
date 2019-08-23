939fe4f3b581e9713f8c089624b9edffcbd24e9f
Amber Brasher <abrashe@toronto.ca>
2019-08-23 15:45:18 -0400

    Created file to download gql schema

---
+++ src/apollo/lib/schema.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const { introspectionQuery } = require('graphql');
+const ProxyAgent = require('proxy-agent');
+const fs = require('fs');
+
+const fun = async () => {
+  const response = await fetch('https://tw-datamodel.herokuapp.com/v1/graphql', {
+    method: 'POST',
+    agent: new ProxyAgent('http://abrashe:e2yT87641@proxy.toronto.ca:8080'),
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: introspectionQuery }),
+  });
+
+  const text = await response.text();
+  fs.writeFileSync('schema.graphql', text);
+};
+fun();
+
+/*
+ * .then(res => res.json())
+ * .then(res => fs.writeFileSync('result.json', JSON.stringify(res.data, null, 2)));
+ */
