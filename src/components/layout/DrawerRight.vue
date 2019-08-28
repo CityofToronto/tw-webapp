534a7062f13a360604d20ce9b32e45afaa561de6
Amber Brasher <abrashe@toronto.ca>
2019-08-28 15:40:04 -0400

    style(DrawerRight): change code style

--- src/components/layout/DrawerRight.vue
+++ src/components/layout/DrawerRight.vue
@@ -9,22 +9,16 @@
     width="35%"
     stateless
   >
-    <v-card
-      v-for="grid in gridList"
-      :key="grid.tableName"
-      class="mx-2 my-3"
-    >
-      <grid-with-toolbar
-        :grid-type="grid.relation"
-        :table-name="grid.tableName"
-      />
+    <v-card v-for="grid in gridList" :key="grid.tableName" class="mx-2 my-3">
+      <grid-with-toolbar :grid-type="grid.relation" :table-name="grid.tableName" />
     </v-card>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
 import { Vue, Prop, Component } from 'vue-property-decorator';
-import GridWithToolbar, { GridType } from '../grid/GridWithToolbar.vue';
+import { GridType } from '@/types/grid';
+import GridWithToolbar from '@/components/GridWithToolbar.vue';
 import apolloClient from '@/apollo';
 
 @Component({
@@ -35,7 +29,7 @@ import apolloClient from '@/apollo';
 export default class DrawerRight extends Vue {
   @Prop(Boolean) readonly rightSideDrawer!: boolean;
 
-  gridList: {tableName: string; relation: GridType}[] = [];
+  gridList: { tableName: string; relation: GridType }[] = [];
 
   async created() {
     const relationships = await apolloClient.getRelationships(this.$route.params.table);
@@ -64,7 +58,7 @@ export default class DrawerRight extends Vue {
         }
       });
   }
-};
+}
 </script>
 
 <style scoped>
