25e0e8150f8d8b6c17d25260ac74f7690fb19007
Amber Brasher <abrashe@toronto.ca>
2019-09-05 15:06:24 -0400

    feat(AliasCell): new component
    names into aliasProperty to display
    keeps id as the real value in the logic

---
+++ src/components/grid/ag-components/AliasCell.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>
+    {{ cellValue }}
+  </span>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { ICellRendererParams } from 'ag-grid-community';
+
+export interface AliasCellParams extends ICellRendererParams {
+  aliasColumn: string;
+}
+
+/**
+ * This component maps the id to another column value
+ * Used in grouping where we group by id but display the name to the user
+ */
+@Component({})
+export default class AliasCell extends Vue {
+  params!: AliasCellParams;
+
+  get cellValue(): string {
+    return this.params.data[this.params.aliasColumn] || 'unknown';
+  }
+}
+</script>
+
+<style></style>
