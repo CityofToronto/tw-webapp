b45fb074897b46f2038b7878fbe3044843c7f17a
Amber Brasher <abrashe@toronto.ca>
2019-09-20 13:42:55 -0400

    feat(RoleAsset): updated page to include right side bar

--- src/views/TreePage.vue
+++ src/views/TreePage.vue
@@ -1,12 +1,27 @@
 <template>
-  <v-layout column wrap fill-height px-4 py-2>
-    <v-flex grow>
+  <v-layout row wrap fill-height px-4 py-2>
+    <v-flex md6 lg8>
       <v-sheet height="100%" elevation="2">
         <grid-with-toolbar
-          :table-name="'WORK_TYPE'"
+          :table-name="tableName"
           :grid-type="gridTypes.Tree"
         />
       </v-sheet>
+    </v-flex>
+    <v-flex md6 lg4>
+      <v-layout column fill-height>
+        <v-flex xs6 px-2>
+          <v-sheet height="100%" elevation="2">
+            <grid-with-toolbar
+              :tableName="'inactive_asset'"
+              :grid-type="gridTypes.Direct"
+            />
+          </v-sheet>
+        </v-flex>
+        <v-flex xs6>
+          Bottom
+        </v-flex>
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
@@ -22,7 +37,7 @@ import { GridType } from '@/types/grid';
   },
 })
 export default class TreePage extends Vue {
-  tableName: string = 'WORK_TYPE';
+  tableName: string = 'role_asset';
   gridTypes = GridType;
 }
 </script>
