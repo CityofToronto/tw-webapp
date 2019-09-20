d57dca0543ba12d2e2df4dd8c982ea4cdd3fc4e7
Amber Brasher <abrashe@toronto.ca>
2019-09-20 13:33:12 -0400

    chore(ESLint): updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,35 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: [
-     '@typescript-eslint'
-  ],
-  extends: [
-    'plugin:vue/essential',
-    '@vue/prettier',
-    '@vue/typescript',
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-extra-semi': 'off',
-    "no-useless-constructor": "off",
-    "indent": "off",
-    "import/no-unresolved": "off",
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/indent": ["off", 2],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/member-delimiter-style": "off"
   },
-  parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    extraFileExtensions: [
-      '.vue',
-    ],
   },
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    '@vue/typescript'
+  ]
 };
