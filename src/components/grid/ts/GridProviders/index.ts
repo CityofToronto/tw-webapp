b19d7acfc679395df4c5291dc21fb1fa4805c1a6
Amber Brasher <abrashe@toronto.ca>
2019-09-20 13:50:02 -0400

    refactor: changed import path

--- src/components/grid/ts/GridProviders/index.ts
+++ src/components/grid/ts/GridProviders/index.ts
@@ -1,3 +1,3 @@
-export { DirectProvider } from './DirectProvider';
-export { OTMProvider } from './OTMProvider';
-export { MTMProvider } from './MTMProvider';
+export { DirectProvider } from './Providers/DirectProvider';
+export { OTMProvider } from './Providers/OTMProvider';
+export { MTMProvider } from './Providers/MTMProvider';
