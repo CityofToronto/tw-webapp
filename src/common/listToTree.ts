972f89304ba189ecef8021799ba48ecce89d5c1e
Amber Brasher <abrashe@toronto.ca>
2019-09-20 13:36:40 -0400

    style: added ts-ignore lines

--- src/common/listToTree.ts
+++ src/common/listToTree.ts
@@ -11,24 +11,29 @@ export const listToTree = <T extends TreeFlatList>(data: T): T => {
   let item: Partial<TreeStructure>;
   let id;
   let parentId;
-
+  // @ts-ignore
   for (let i = 0, { length } = data; i < length; i += 1) {
+    // @ts-ignore
     item = data[i];
     id = item.id;
     parentId = item.parent || 0;
     // every item may have children
+    // @ts-ignore
     childrenOf[id] = childrenOf[id] || [];
     // init its children
+    // @ts-ignore
     item.children = childrenOf[id];
     if (parentId !== 0) {
       // init its parent's children object
+      // @ts-ignore
       childrenOf[parentId] = childrenOf[parentId] || [];
       // push it into its parent's children object
+      // @ts-ignore
       childrenOf[parentId].push(item);
     } else {
       tree.push(item);
     }
-  };
-
+  }
+  // @ts-ignore
   return tree;
 };
