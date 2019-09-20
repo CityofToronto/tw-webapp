f8374a6fa90bf2aaea20a312804ec5a90d0818bf
Amber Brasher <abrashe@toronto.ca>
2019-09-20 13:48:54 -0400

    feat(heirarchy): added support for rearrangement

--- src/components/grid/TreeGrid.vue
+++ src/components/grid/TreeGrid.vue
@@ -1,3 +1,4 @@
+/* eslint-disable vue/no-parsing-error */ /* eslint-disable vue/valid-v-bind */
 <template>
   <ag-grid-vue
     :style="
@@ -11,19 +12,28 @@
     :header-height="7 * 7"
     :context="context"
     :animate-rows="true"
-    :pagination="true"
+    :pagination="pagination"
     :pagination-auto-page-size="true"
-    :auto-group-column-def="autoGroupColumnDef"
     @grid-ready="onGridReady"
     @row-drag-move="onRowDragMove"
     @row-drag-end="onRowDragEnd"
+    @row-drag-leave="onRowDragLeave"
+    @row-group-opened="onGroupOpened"
+    @cell-value-changed="cellValueChanged"
   />
 </template>
 
 <script lang="ts">
-import { RowDragEnterEvent, RowDragMoveEvent, ColDef } from 'ag-grid-community';
+import {
+  RowDragEnterEvent,
+  RowDragMoveEvent,
+  RowNode,
+  ICellRendererParams,
+  RowGroupOpenedEvent,
+} from 'ag-grid-community';
 import { Component, Mixins } from 'vue-property-decorator';
 import GridMixin from './ts/GridMixin';
+import { RowData } from '@/types/grid';
 import TreeTransformer from './ts/GridProviders/GridTransformer/TreeTransformer';
 import AliasCell from '@/components/grid/ag-components/AliasCell.vue';
 
@@ -32,36 +42,140 @@ import AliasCell from '@/components/grid/ag-components/AliasCell.vue';
     AliasCell,
   },
 })
-export default class GridComponent extends Mixins(GridMixin) {
-  autoGroupColumnDef: ColDef = {
-    resizable: true,
-    rowDrag: true,
-    cellRendererParams: {
-      aliasColumn: 'name',
-      innerRendererFramework: AliasCell,
-    },
-  };
+export default class TreeGridComponent extends Mixins(GridMixin) {
+  potentialParent!: RowNode | null;
+
+  openRowGroups: string[] = [];
 
   async created() {
+    this.customColDefs = {
+      cellClassRules: {
+        'hover-over': (params: ICellRendererParams) =>
+          params.node === this.potentialParent,
+      },
+    };
+
+    this.gridInitializedEvent = () => {
+      this.gridInstance.setGridUpdateEvent((rowData: RowData[]) => {
+        rowData
+          .filter((row): boolean => this.openRowGroups.includes(`${row.id}`))
+          .forEach((row) => {
+            this.gridApi.getRowNode(`${row.id}`).setExpanded(true);
+          });
+      });
+    };
+
     this.dataTransformer = new TreeTransformer();
     this.gridOptions = {
       ...this.gridOptions,
+      groupSelectsChildren: false,
       treeData: true,
       // dataItem.group is populated in the TreeTransformer
       // boolean to show whether or not there are children
       isServerSideGroup: (dataItem): boolean => dataItem.group,
       // What column to group by, here it is the id column as it should
       // always be present in the data
-      getServerSideGroupKey: (dataItem): string => dataItem.id,
+      getServerSideGroupKey: (dataItem): string => `${dataItem.id}`,
+      suppressRowClickSelection: true,
+      autoGroupColumnDef: {
+        resizable: true,
+        rowDrag: true,
+        width: 400,
+        cellRendererParams: {
+          aliasColumn: 'name',
+          checkbox: true,
+          innerRendererFramework: AliasCell,
+        },
+        cellClassRules: {
+          'hover-over': (params: ICellRendererParams) =>
+            params.node === this.potentialParent,
+        },
+      },
     };
   }
 
-  onRowDragMove(params: RowDragMoveEvent) {
-    console.log('Row Moving');
+  onRowDragMove(event: RowDragMoveEvent) {
+    this.setPotentialParent(event.overNode);
   }
 
-  onRowDragEnd(params: RowDragEnterEvent) {
-    console.log('Row Drag Ending', params);
+  onRowDragLeave() {
+    // Remove all highlighting when drag leaves the grid
+    this.setPotentialParent(null);
+  }
+
+  onRowDragEnd(event: RowDragEnterEvent) {
+    const rowToMove = event.node.data;
+    const newParentId = event.overNode ? event.overNode.data.id : null;
+    if (newParentId === rowToMove.id) {
+      return;
+    }
+
+    const newData = {
+      ...rowToMove,
+      parent: newParentId,
+    };
+    this.gridInstance.updateRows({
+      rowsToUpdate: [newData],
+      successCallback: () => {
+        this.gridApi.purgeServerSideCache();
+        // this.refreshBranch(event.node)
+        // this.refreshBranch(event.overNode)
+      },
+    });
+  }
+
+  onGroupOpened(event: RowGroupOpenedEvent) {
+    this.openRowGroups.push(event.node.id.toString());
+  }
+
+  launchFormAdder(rowNode: RowNode) {
+    this.$emit('add', rowNode, { parent: rowNode.data.id });
+  }
+
+  refreshBranch(node: RowNode) {
+    const getParent = (parentId: string): RowNode => {
+      return this.gridApi.getRowNode(parentId).parent as RowNode;
+    };
+    let branch: string[] = [node.data.id];
+    let parentNode: RowNode = node.parent as RowNode;
+    while (parentNode.level > 0) {
+      branch.push(parentNode.id);
+      if (parentNode.parent) {
+        parentNode = getParent(parentNode.id);
+      }
+    }
+    this.gridApi.purgeServerSideCache(branch.reverse());
+  }
+
+  setPotentialParent(overNode: RowNode | null) {
+    // This validates parents, updates the highlighting by refreshing the new and old cells
+    let newPotentialParent: RowNode | null;
+    if (overNode) {
+      newPotentialParent = overNode;
+    } else {
+      newPotentialParent = null;
+    }
+    if (this.potentialParent === newPotentialParent) {
+      return;
+    }
+    let rowsToRefresh: RowNode[] = [];
+    if (this.potentialParent) {
+      rowsToRefresh.push(this.potentialParent);
+    }
+    if (newPotentialParent) {
+      rowsToRefresh.push(newPotentialParent);
+    }
+    this.potentialParent = newPotentialParent;
+    this.gridApi.refreshCells({
+      rowNodes: rowsToRefresh,
+      force: true,
+    });
   }
 }
 </script>
+
+<style>
+.hover-over {
+  background-color: #e5e5ff;
+}
+</style>
