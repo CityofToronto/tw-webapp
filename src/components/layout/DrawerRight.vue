948cd0176e2e67319b627c7e1fb6f2fc97bdbe38
Amber Brasher <abrashe@toronto.ca>
2019-10-22 11:34:04 -0400

    refactor: disabled broken code

--- src/components/layout/DrawerRight.vue
+++ src/components/layout/DrawerRight.vue
@@ -9,12 +9,12 @@
     width="35%"
     stateless
   >
-    <v-card v-for="grid in gridList" :key="grid.tableName" class="mx-2 my-3">
-      <!-- <grid-with-toolbar
+    <!-- <v-card v-for="grid in gridList" :key="grid.tableName" class="mx-2 my-3">
+      <grid-with-toolbar
         :grid-type="grid.relation"
         :table-name="grid.tableName"
-      /> -->
-    </v-card>
+      />
+    </v-card> -->
   </v-navigation-drawer>
 </template>
 
@@ -38,34 +38,33 @@ export default class DrawerRight extends Vue {
     // const relationships = await apolloClient.getRelationships(
     //   this.$route.params.table,
     // );
-
     // Filter for table relationships
-    this.gridList = relationships
-      .filter((relationship): boolean =>
-        relationship.name.includes('aggregate'),
-      )
-      .map((relationship) => {
-        /**
-         *  Two options, activity_legislation_aggregate returns length of 3 when split by _ (MTM)
-         *  trade_aggregate returns length of 2 when split by _ (OTM)
-         */
-        switch (relationship.name.split('_').length) {
-          case 2:
-            return {
-              tableName: relationship.name.split('_')[0],
-              relation: GridType.OneToMany,
-            };
-          case 3:
-            return {
-              tableName: relationship.name.split('_')[1],
-              relation: GridType.ManyToMany,
-            };
-          default:
-            throw new Error(
-              `Incorrect relationship schema for ${this.$route.params.table}`,
-            );
-        }
-      });
+    // this.gridList = relationships
+    //   .filter((relationship): boolean =>
+    //     relationship.name.includes('aggregate'),
+    //   )
+    //   .map((relationship) => {
+    //     /**
+    //      *  Two options, activity_legislation_aggregate returns length of 3 when split by _ (MTM)
+    //      *  trade_aggregate returns length of 2 when split by _ (OTM)
+    //      */
+    //     switch (relationship.name.split('_').length) {
+    //       case 2:
+    //         return {
+    //           tableName: relationship.name.split('_')[0],
+    //           relation: GridType.OneToMany,
+    //         };
+    //       case 3:
+    //         return {
+    //           tableName: relationship.name.split('_')[1],
+    //           relation: GridType.ManyToMany,
+    //         };
+    //       default:
+    //         throw new Error(
+    //           `Incorrect relationship schema for ${this.$route.params.table}`,
+    //         );
+    //     }
+    //   });
   }
 }
 </script>
