3ed2f20b671c05a7b7716e88e8cfd5890f3813b0
Amber Brasher <abrashe@toronto.ca>
2019-10-22 09:09:47 -0400

    refactor: renamed imports

--- src/config/grids.ts
+++ src/config/grids.ts
@@ -6,7 +6,7 @@ import agComponents from '@/components/grid/ag-components';
 import * as toolbarItems from '@/components/grid/ts/toolbarItems';
 import * as contextItems from '@/components/grid/ts/contextItems';
 import * as gridEvents from '@/components/grid/ts/gridEvents';
-import * as gridButtons from '@/components/grid/ts/ColumnDefiner/gridButtons';
+import * as gridButtons from '@/components/grid/ts/ColumnFactory/gridButtons';
 import { ICellRendererParams } from 'ag-grid-community';
 
 export const gridConfig: GridConfigurationInterface = {
