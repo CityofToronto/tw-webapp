706f0cb4a0bc3aec86f7e75e6e0bdfc64ea2284b
Amber Brasher <abrashe@toronto.ca>
2019-10-22 09:06:59 -0400

    feat(Nav Drawer): remade navigation drawer
    added support for admin area
    changed styling

--- src/components/layout/NavigationDrawer.vue
+++ src/components/layout/NavigationDrawer.vue
@@ -9,44 +9,54 @@
     stateless
   >
     <v-list dense>
-      <v-list-group
-        v-for="mod in modules"
-        :key="mod.title"
-        :prepend-icon="mod.icon"
-        value="true"
-        no-action
-      >
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title v-text="mod.title"></v-list-item-title>
-          </v-list-item-content>
-        </template>
-        <v-list-item
-          v-for="item in mod.items"
-          :key="item.title"
-          :to="item.route"
+      <div v-for="mod in modules" :key="mod.title">
+        <v-list-group
+          v-if="!mod.admin || showAdminPanel"
+          :prepend-icon="mod.icon"
+          value="true"
+          no-action
         >
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="mod.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item
+            v-for="item in mod.items"
+            :key="item.title"
+            :to="item.route"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </div>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import Store from '@/store/store';
+import { useStore } from 'vuex-simple';
 
 @Component({})
 export default class NavigationDrawer extends Vue {
   @Prop(Boolean) readonly leftSideDrawer: boolean = false;
+
+  store: Store = useStore(this.$store);
+
+  get showAdminPanel() {
+    return this.store.auth.user.username === 'tony.huang';
+  }
 
   modules = [
     {
       icon: 'assignment',
       title: 'Project Asset',
       active: true,
+      admin: false,
       items: [
         { title: 'Reservation', route: '/assets/reservation' },
         { title: 'Record Reconciliation', route: '/assets/reconciliation' },
@@ -55,6 +65,7 @@ export default class NavigationDrawer extends Vue {
     },
     {
       icon: 'person',
+      admin: true,
       title: 'Administration',
       active: true,
       items: [{ title: 'Reservation Approval', route: '/admin/approval' }],
