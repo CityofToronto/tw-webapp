5f33007dbefbb516521fb6099a1fcf8754528add
Amber Brasher <abrashe@toronto.ca>
2019-10-24 15:58:29 -0400

    update

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,15 +1,14 @@
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 const msg = {
-    to: 'amber.brasher@toronto.ca',
-    from: 'sender@example.org',
-    subject: 'Hello world',
-    text: 'Hello plain world!',
-    html: '<p>Hello HTML world!</p>',
+  to: 'amber.brasher@toronto.ca.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 
-sgMail
-    .send(msg)
-    .then(() => console.log('Mail sent successfully'))
-    .catch(error => console.error(error.toString()));
+sgMail.send(msg).then(() => console.log('Sent Mail!'));
